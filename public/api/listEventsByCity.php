3d75b7389e45d98cf8adb78b77b805fea8129444
steelmace23 <marcosmiguel96@yahoo.com.ar>
2020-11-29 04:42:08 -0500

    another test

--- public/api/listEventsByCity.php
+++ public/api/listEventsByCity.php
@@ -30,7 +30,7 @@ else
         $date = date("Y-m-d h:i:s", time());
 
         // SELECT * FROM `events` WHERE `city` like 'orlando' and (`start_time` <= '2020-11-29') and (`end_time` >= '2020-11-29')
-        $sql .= " where `city` like '%" . $city . "%' ";
+        $sql .= " where `city` like '%" . $city . "%' and (`start_time` <= '2020-11-29') and (`end_time` >= '2020-11-29')";
         // and (`start_time` <= '%" . $date . "%') and (`end_time` >= '%" . $date . "%')";
     }
     else 
