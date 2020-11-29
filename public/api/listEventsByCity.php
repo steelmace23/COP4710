355fb41e7155100002f37c42b05588d6ee6b429c
steelmace23 <marcosmiguel96@yahoo.com.ar>
2020-11-29 04:20:29 -0500

    fixing query

--- public/api/listEventsByCity.php
+++ public/api/listEventsByCity.php
@@ -27,8 +27,10 @@ else
     // UserID is required in order to search the current user's contacts
     if ($city != '') 
     {
+        $date = date("Y-m-d h:i:s", time());
+
         // SELECT * FROM `events` WHERE `city` like 'orlando' and (`start_time` <= '2020-11-29') and (`end_time` >= '2020-11-29')
-        $sql .= "where `city` like '%" . $city . "%' and `start_time` <= '%" . date("Y-m-d h:i:s", time()) . "%' and `end_time` >= '%" . date("Y-m-d h:i:s", time()) . "%'";
+        $sql .= " where `city` like '%" . $city . "%' and (`start_time` <= '%" . $date . "%') and (`end_time` >= '%" . $date . "%')";
     }
     else 
     {
