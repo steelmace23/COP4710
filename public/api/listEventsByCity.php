0d21fba8951078089fcb7f68d8e8737210a2eedf
steelmace23 <marcosmiguel96@yahoo.com.ar>
2020-11-29 04:50:56 -0500

    can i get a string tho?

--- public/api/listEventsByCity.php
+++ public/api/listEventsByCity.php
@@ -27,7 +27,7 @@ else
     // UserID is required in order to search the current user's contacts
     if ($city != '') 
     {
-        $date = date("Y-m-d");
+        $date = date('Y-m-d h:i:s');
 
         // SELECT * FROM `events` WHERE `city` like 'orlando' and (`start_time` <= '2020-11-29') and (`end_time` >= '2020-11-29')
         $sql .= " where `city` like '%" . $city . "%' and (`start_time` <= '%" . $date . "%') and (`end_time` >= '%" . $date . "%')";
