59b70bed058c2ca2049e5c37c77f20c2e0cb2376
Keenan Turley <keenan.turley@gmail.com>
2020-12-01 14:49:09 -0500

    Refactor listEventsByAdmin.php

--- public/api/listEventsByAdmin.php
+++ public/api/listEventsByAdmin.php
@@ -9,99 +9,79 @@ if ($_SERVER['REQUEST_METHOD'] !== 'GET') {
     error(400, 'Invalid request method');
 }
 
+// Filter possible inputs
 $username = filter_input(INPUT_GET, 'username');
+$user_id = filter_input(INPUT_GET, 'user_id', FILTER_VALIDATE_INT);
+$onlyActive = filter_input(INPUT_GET, 'onlyActive', FILTER_FILTER_VALIDATE_BOOLEAN);
 
+// Validate inputs
+if (!$username && !$user_id) {
+    error(400, 'Invalid request parameters: one of username or user_id required');
+}
+
+// Connect to the MySQL server
 $db_user = getenv('DB_USER');
 $db_pass = getenv('DB_PASS');
-$conn = new mysqli('localhost', $db_user, $db_pass, 'event_portal');
-if ($conn->connect_error) 
-{
-    returnWithError( $conn->connect_error );
-} 
-else
-{
-    // Create an sql form to send to databse
-    $sql = "select * from `users`" ;
+$mysqli = new mysqli('localhost', $db_user, $db_pass, 'event_portal');
+if ($mysqli->connect_errno) {
+    error(500, 'Failed to connect to database');
+}
 
-    // UserID is required in order to search the current user's contacts
-    if ($username != '') 
-    {
-        $sql .= "where `username` like '$username'";
+// If given the username, resolve it to the user_id
+if ($username) {
+    $user_id = getUserIdFromDB($mysqli, $username); 
+}
+
+// Build the query
+$query = 'SELECT * FROM `events` WHERE `admin_id` = ?';
+
+if ($onlyActive) {
+    $query = $query . ' AND `start_time` <= NOW() AND `end_time` > NOW()';
+}
+
+$query = $query . ' ORDER BY `start_time`';
+
+// Prepare the query
+if (!($stmt = $mysqli->prepare($query))) {
+    error(500, 'Failed to prepare query');
+}
+
+// Bind parameter before execution
+$stmt->bind_param('i', $user_id);
+
+$success = $stmt->execute();
+if (!$success) {
+    error(500, 'Error querying database for events');
+}
+
+$result = $stmt->get_result();
+$rows = $result->fetch_all(MYSQLI_ASSOC);
+
+$response = [ 'events' => $rows ];
+
+header('Content-Type: application/json');
+echo json_encode($response);
+
+$stmt->close();
+
+
+function getUserIdFromDB($mysqli, $username) {
+    // Get the user row by username
+    $query = 'SELECT `user_id` FROM `users` WHERE `username` = ?';
+    if (!($stmt = $mysqli->prepare($query))) {
+        error(500, 'Failed to prepare query');
     }
-    else 
-    {
-        returnWithError('NO USERNAME');
-    }       
-        
-    $result = $conn->query($sql);       
 
-    if ($result->num_rows > 0)
-    {
-        $row = $result->fetch_assoc();
-        $admin_id = $row["user_id"];
-
-        // Create an sql form to send to databse
-        $sql = "select * from `events`" ;
-
-        // UserID is required in order to search the current user's contacts
-        if ($admin_id != '') 
-        {
-            $sql .= "where `admin_id` =" . $admin_id;
-        }
-        else 
-        {
-            returnWithError('NO ADMIN ID');
-        }       
-    
-        $result = $conn->query($sql);
-        
-        if ($result->num_rows > 0)
-        {        
-            $searchResults = array();
-
-            while($row = $result->fetch_assoc())
-            {
-                $searchResults[] = $row;
-            }
-            
-            returnWithInfo( $searchResults );
-        }
-        else
-        {
-            returnWithError( "No Records Found" );
-        }
-        $conn->close();
-        
+    $stmt->bind_param('s', $username);
+    $success = $stmt->execute();
+    if (!$success) {
+        error(500, 'Error querying database for given user');
     }
-    else
-    {
-        returnWithError( "No Records Found" );
-    }       
-}
 
-function getRequestInfo()
-{
-    return json_decode(file_get_contents('php://input'), true);
-}
+    $result = $stmt->get_result();
+    if ($result->num_rows === 0) {
+        error(404, 'No user with given username found');
+    }
 
-function sendResultInfoAsJson( $obj )
-{
-    echo json_encode($obj);
-}
-
-function returnWithSuccess( $success )
-{
-    $retValue = '{"success":"' . $success . '"}';
-    sendResultInfoAsJson( $retValue );
-}
-
-function returnWithError( $err )
-{
-    $retValue = '{"error":"' . $err . '"}';
-    sendResultInfoAsJson( $retValue );
-}
-
-function returnWithInfo( $searchResults )
-{
-    sendResultInfoAsJson( $searchResults );
+    return $result->fetch_assoc()['user_id'];
 }
