a105f442965c8eb4203d538cc5635f72d36dd657
itsemmie <44448720+itsemmie@users.noreply.github.com>
2020-12-05 21:22:18 -0500

    tabulator added to superAdmin

---
+++ public/js/superAdmin.js
@@ -0,0 +1,92 @@
+const apiPath ='https://eventportal.para.cx/api';
+
+
+   const username = 'keenan';
+   const password = 'testing';
+
+const loadP = async () => {
+  const endpoint = 'listEvents.php'
+  const requestPath = `${apiPath}/${endpoint}`;
+  
+  //Authorization
+  const createAuthorizationHeader = (username, password) => {
+      // I use template literal here
+      const credential = btoa(`${username}:${password}`);
+      const authValue = `Basic ${credential}`;
+      return new Headers({ 'Authorization': authValue });
+    }
+
+  const response = await fetch(requestPath, {
+        method: 'GET', // Change this on POST requests
+		headers: createAuthorizationHeader(username, password)
+    });
+    // We need to await here as well because .json() returns a Promise
+    const data = await response.json();
+
+    // Response code is outside HTTP 200-299, therefore its an error
+    if (!response.ok) {
+        return console.log(`Error: ${data.error}`);
+    }
+	console.log(data.event);
+	console.log(data);
+	
+		var table = new Tabulator("#supAdmin-table", {
+	height:"311px",
+	data: data.events,
+	layout:"fitColumns",
+    columns:[                 //define the table columns
+        {title:"Event", field:"title"},
+        {title:"Description", field:"description", hozAlign:"left"},
+        {title:"URL", field:"url"},
+        {title:"Start Time", field:"start_time"},
+        {title:"End Time", field:"end_time"},
+        {title:"Location", field:"city"},
+        
+    ],
+});
+	
+}
+loadP();
+
+const search = async () => {
+  const endpoint = 'listEvents.php'
+  const requestPath = `${apiPath}/${endpoint}`;
+  
+  //Authorization
+  const createAuthorizationHeader = (username, password) => {
+      // I use template literal here
+      const credential = btoa(`${username}:${password}`);
+      const authValue = `Basic ${credential}`;
+      return new Headers({ 'Authorization': authValue });
+    }
+
+  const response = await fetch(requestPath, {
+        method: 'GET', // Change this on POST requests
+		headers: createAuthorizationHeader(username, password)
+    });
+    // We need to await here as well because .json() returns a Promise
+    const data = await response.json();
+
+    // Response code is outside HTTP 200-299, therefore its an error
+    if (!response.ok) {
+        return console.log(`Error: ${data.error}`);
+    }
+	console.log(data.event);
+	console.log(data);
+	
+		var table = new Tabulator("#supAdmin-table", {
+	height:"311px",
+	data: data.events,
+	layout:"fitColumns",
+    columns:[                 //define the table columns
+        {title:"Event", field:"title"},
+        {title:"Description", field:"description", hozAlign:"left"},
+        {title:"URL", field:"url"},
+        {title:"Start Time", field:"start_time"},
+        {title:"End Time", field:"end_time"},
+        {title:"Location", field:"city"},
+        
+    ],
+});
+	
+}
