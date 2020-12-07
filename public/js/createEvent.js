16b5cfdc201b59b576b05a9fb4c9c3e1455320fb
Keenan Turley <keenan.turley@gmail.com>
2020-12-07 00:26:46 -0500

    Refactor create event page

--- public/js/createEvent.js
+++ public/js/createEvent.js
@@ -1,52 +1,92 @@
-const apiPath ='https://eventportal.para.cx/api';
+import API from './api.js';
+import { getUser, validateForm } from './util.js';
 
+// Async wrapper - I wish I didn't have to do this
+(async () => {
 
-	 const user = {
-        username: 'keenan',
-        password: 'testing',
-        user_id: 31
-    };
-	
-    const onClickCreate = async () => {
-		const endpoint = 'createEvent.php'
-		const requestPath = `${apiPath}/${endpoint}`;
-		
-		
-	var title = document.getElementById("event-title").value;
-	var description = document.getElementById("description").value;
-	var url = document.getElementById("url").value;
-	var start_time = document.getElementById("start").value+"Z";
-	var end_time = document.getElementById("end").value+"Z";
-	var address = document.getElementById("address").value;
-	var address2 = document.getElementById("address2").value;
-	var city = document.getElementById("city").value;
-	var state = document.getElementById("state").value;
-	var postal = document.getElementById("postalcode").value;
-	
-const createAuthorizationHeader = (username, password) => {
-      // I use template literal here
-      const credential = btoa(`${username}:${password}`);
-      const authValue = `Basic ${credential}`;
-      return new Headers({ 'Authorization': authValue });
+    // Redirect the user to the login page if they're not logged in
+    const user = getUser();
+    if (!user) {
+        window.location.href = './login.html';
     }
 
-	var jsonPayload = '{"admin_id" : "' + user.user_id + '", "title" : "' + title + '", "description" : "' + description + '", "url" : "' + url + '", "start_time" : "' + start_time + '", "end_time" : "' + end_time + '", "address" : "' + address + '", "address2" : "' + address2 + '", "city" : "' + city + '", "state" : "' + state + '", "postal_code" : "' + postal + '"}';
-	
-	const response = await fetch(requestPath, {
-        method: 'POST', // Change this on POST requests
-		headers: createAuthorizationHeader(user.username, user.password),
-		body: jsonPayload
+    const logoutButton = document.getElementById('logout-button');
+    logoutButton.addEventListener('click', () => {
+        logoutUser();
+        window.location.href = './login.html';
     });
-    // We need to await here as well because .json() returns a Promise
-    const data = await response.json();
 
-    // Response code is outside HTTP 200-299, therefore its an error
-    if (!response.ok) {
-        return console.log(`Error: ${data.error}`);
+    // Only show the super admin link if the user is a superadmin
+    if (user.is_superadmin) {
+        document.getElementById('super-admin-link').classList.remove('d-none');
     }
-	console.log(data.event);
-	console.log(data);
-	
-	if(alert('Event Added Succesfully!')){}
-	else    window.location.reload(); 
+})();
+
+const user = getUser();
+
+const createEventResponseElement = document.getElementById('create-event-response');
+const form = document.getElementById('create-event-form');
+
+// Attach a listener to the create event button to enable event creation behavior
+const createEventButton = document.getElementById('create-event-button');
+createEventButton.addEventListener('click', async () => {
+    // Check validity
+    if (!validateForm(form)) {
+        return;
     }
+
+    const plusIcon = document.getElementById('create-event-plus');
+    const spinner = document.getElementById('create-event-spinner');
+
+    // Disable create event button
+    createEventButton.setAttribute('disabled', true);
+    plusIcon.classList.toggle('d-none');
+    spinner.classList.toggle('d-none');
+
+    // Transform the dates and times into datetimes
+    const startDate = document.getElementById('start-date').value;
+    const startTime = document.getElementById('start-time').value;
+    const startDateTime = `${startDate}T${startTime}:00Z`;
+    const endDate = document.getElementById('end-date').value;
+    const endTime = document.getElementById('end-time').value;
+    const endDateTime = `${endDate}T${endTime}:00Z`;
+
+
+    // Grab input values
+    const event = {
+        admin_id: user.user_id,
+        title: document.getElementById("event-title").value,
+        description: document.getElementById("description").value,
+        url: document.getElementById("url").value,
+        start_time: startDateTime,
+        end_time: endDateTime,
+        address: document.getElementById("address").value,
+        address2: document.getElementById("address-2").value,
+        city: document.getElementById("city").value,
+        state: document.getElementById("state").value,
+        postal_code: document.getElementById("postal-code").value
+    }
+
+    // Create an event with the given information
+    const response = await API.createEvent(user, event);
+    console.log(response);
+
+    if (response.error) {
+        createEventResponseElement.classList.add('alert-danger');
+        createEventResponseElement.classList.remove('alert-success');
+        createEventResponseElement.innerText = `Error: ${response.error}`;
+    } else {
+        createEventResponseElement.classList.remove('alert-danger');
+        createEventResponseElement.classList.add('alert-success');
+        createEventResponseElement.innerText = "Event created successfully!"
+    }
+
+    createEventResponseElement.classList.remove('d-none');
+
+    // Re-enable create event button
+    createEventButton.removeAttribute('disabled');
+    plusIcon.classList.toggle('d-none');
+    spinner.classList.toggle('d-none');
+});
+
+
